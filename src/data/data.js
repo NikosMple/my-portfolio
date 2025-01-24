e0e0643b9615361a1c62e0225e0088560a0591bf
nikos <nikosmple1998@gmail.com>
2025-01-24 12:21:28 +0200

    UPDATE PROJECTS

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,8 @@
 import keeper from "../assets/Projects/react-keeper.png";
 import top100 from "../assets/Projects/crypto-top.png";
 import keeperVol2 from "../assets/Projects/keepervol2.png";
+import quizApp from "../assets/Projects/quiz-app.png";
+
 
 
 export const data=[
@@ -11,9 +13,16 @@ export const data=[
         github:"https://github.com/NikosMple/Keeper-FullStack",
         hasLiveButton: false,
     },
-
     {
         id:2,
+        name:"Full Stack Quiz-App",
+        image:quizApp,
+        github:"https://github.com/NikosMple/Capital-Quiz-Game",
+        hasLiveButton: true,
+        live:"https://capital-quiz-game-frontend.onrender.com/",
+    },
+    {
+        id:3,
         name:"React Keeper",
         image:keeper,
         github:"https://github.com/NikosMple/react-keeper",
@@ -21,7 +30,7 @@ export const data=[
         live:"https://nikosmple.github.io/react-keeper/",
     },
     {
-        id:3,
+        id:4,
         name:"Top 100 Crypto Live",
         image:top100,
         github:"https://github.com/NikosMple/top100-crypto",
